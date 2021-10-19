afbc51e3e2e4aa472b291c8098bbf500ca7edeb5
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-19 01:46:36 -0400

    commit tree 7f4efec198a52f55667a2cb38a96b80d3a6e591c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// set up a DOMcontent event listener
+//use event call back function DOMContentLoaded
+// target index with the
+
+
+// function newText(str) {
+//     let text = document.querySelector('p')
+//     text.textContent = "This is really cool!"
+// }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+ let newText = document.getElementById('text')
+ newText.textContent = "This is really cool!"
+ console.log(newText.textContent)
+});
+
+console.log('This console.log() fires when index.js loads - before DOMContentLoaded is triggered')
+
